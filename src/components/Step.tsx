a43e7cbc2462b3072fde53cff6346fc043df6416
DingxuanZhou <zdx700731@gmail.com>
2024-05-01 14:45:33 -0400

    update button

--- src/components/Step.tsx
+++ src/components/Step.tsx
@@ -1,18 +1,17 @@
-import { ReactNode } from "react"
-import { NavigationButtons } from "./NavigationButtons"
+import { ReactNode } from "react";
+import { NavigationButtons } from "./NavigationButtons";
 
 type StepProps = {
-  children: ReactNode
-  back: string
-  next: string
-  home?: boolean
-}
+  children: ReactNode;
+  back: string;
+  next: string;
+};
 
-export const Step = ({ children, back, next, home }: StepProps) => {
+export const Step = ({ children, back, next }: StepProps) => {
   return (
-    <div className='flex flex-col justify-between min-w-[500px] min-h-[200px]'>
+    <div className="flex flex-col justify-between min-w-[500px] min-h-[200px]">
       {children}
-      <NavigationButtons back={back} next={next} home={home} />
+      <NavigationButtons back={back} next={next} />
     </div>
-  )
-}
+  );
+};
